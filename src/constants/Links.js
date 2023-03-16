3200cac2eb5b7b3a5d1d5b55335d44c83ec38eca
vicma <vmaldaraca@gmail.com>
2023-03-16 15:05:35 -0600

    Fix navigation links

--- src/constants/Links.js
+++ src/constants/Links.js
@@ -1,9 +1,9 @@
 const AdminLinks = [
   { id: 'Home', src: 'Home', path: '/' },
-  { id: 'courses', src: 'Home', path: '/courses' },
+  { id: 'courses', src: 'Courses', path: '/courses' },
   { id: 'Reservation', src: 'Reservations', path: '/reservations' },
   { id: 'new-course', src: ' New Course', path: '/course/new' },
-  { id: 'delete', src: 'Delete Course', path: '/courses/delete' },
+  { id: 'delete', src: 'Delete Courses', path: '/courses/delete' },
 ];
 
 const UserLinks = [
@@ -11,12 +11,12 @@ const UserLinks = [
   { id: 'Courses', src: 'Courses', path: '/courses' },
   { id: 'Reservation', src: 'My Reservations', path: '/reservations' },
   { id: 'new-course', src: ' New Course', path: '/course/new' },
-  { id: 'delete', src: 'Delete Course', path: '/courses/delete' },
+  { id: 'delete', src: 'Delete Courses', path: '/courses/delete' },
 ];
 
 const GuestLinks = [
   { id: 'Home', src: 'Home', path: '/' },
-  { id: 'Courses', src: 'Home', path: '/courses' },
+  { id: 'Courses', src: 'Courses', path: '/courses' },
   { id: 'signup', src: 'Sign up', path: '/signup' },
   { id: 'signin', src: 'Sign In', path: '/auth/login' },
 ];
